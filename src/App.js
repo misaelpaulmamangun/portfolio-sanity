8a1f15992d9434307d987ac54dc1cf802ba8abce
misaelpaulmamangun <misaelpaulmamangun@gmail.com>
2021-06-19 14:06:12 +0800

    added sanity data to projects page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Projects from "./Components/Projects";
-import Post from "./Components/Post";
+import Project from "./Components/Project";
 import Navbar from "./Components/Navbar";
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
         <Route component={Home} path="/" exact />
         <Route component={About} path="/about" />
         <Route component={Projects} path="/projects" />
-        <Route component={Post} path="/post/:slug" />
+        <Route component={Project} path="/post/:slug" />
       </Switch>
     </BrowserRouter>
   );
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,10 +1,41 @@
-import React from "react";
-
+import React, {useState, useEffect} from "react";
+import {Link} from "react-router-dom";
+import sanityClient from '../client.js';
 export default function Projects() {
+  const [projectsData, setProjects] = useState(null);
+
+  useEffect(() => {
+    sanityClient.fetch(
+      `*[_type == "post"] {
+        title,
+        slug,
+        mainImage {
+          asset->{
+            _id,
+            url
+          },
+          alt
+        }
+      }`
+    ).then((data) => {
+      setProjects(data)
+    }).catch(console.error)
+  }, [])
+  console.log(projectsData)
   return (
     <div className="docs-section">
       <div className="container">
         <h1>Projects</h1>
+        {projectsData && projectsData.map((project, index) => (
+          <Link to={"/project/" + project.slug.current} key={project.slug.current}>
+            <div className="card four columns">
+              <img 
+                src={project.mainImage.asset.url}
+                alt={project.mainImage.alt}  
+              />
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
