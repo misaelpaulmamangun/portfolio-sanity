7d779ff940d2d5f029df548629e161928c130494
misaelpaulmamangun <misaelpaulmamangun@gmail.com>
2021-07-17 20:09:36 +0800

    update css

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,12 +1,13 @@
-import React, {useState, useEffect} from "react";
-import {Link} from "react-router-dom";
-import sanityClient from '../client.js';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import sanityClient from "../client.js";
 export default function Projects() {
   const [projectsData, setProjects] = useState(null);
 
   useEffect(() => {
-    sanityClient.fetch(
-      `*[_type == "post"] {
+    sanityClient
+      .fetch(
+        `*[_type == "post"] {
         title,
         slug,
         mainImage {
@@ -17,25 +18,33 @@ export default function Projects() {
           alt
         }
       }`
-    ).then((data) => {
-      setProjects(data)
-    }).catch(console.error)
-  }, [])
-  console.log(projectsData)
+      )
+      .then((data) => {
+        setProjects(data);
+      })
+      .catch(console.error);
+  }, []);
+  console.log(projectsData);
   return (
     <div className="docs-section">
       <div className="container">
         <h1>Projects</h1>
-        {projectsData && projectsData.map((project, index) => (
-          <Link to={"/project/" + project.slug.current} key={project.slug.current}>
+        {projectsData &&
+          projectsData.map((project, index) => (
             <div className="card four columns">
-              <img 
-                src={project.mainImage.asset.url}
-                alt={project.mainImage.alt}  
-              />
+              <Link
+                to={"/project/" + project.slug.current}
+                key={project.slug.current}
+              >
+                <div class="item-img">
+                  <img
+                    src={project.mainImage.asset.url}
+                    alt={project.mainImage.alt}
+                  />
+                </div>
+              </Link>
             </div>
-          </Link>
-        ))}
+          ))}
       </div>
     </div>
   );
