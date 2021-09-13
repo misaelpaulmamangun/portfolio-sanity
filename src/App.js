d1904525ca72948064f8bd9bd345133891e6640e
Misael Paul Mamangun <misaelpaulmamangun@gmail.com>
2021-09-13 13:09:33 +0800

    update extension

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import "./App.css";
 import "react-skeleton-css/styles/skeleton.2.0.4.css";
 import "react-skeleton-css/styles/normalize.3.0.2.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from "./Components/Home";
-import About from "./Components/About";
-import Projects from "./Components/Projects";
-import Project from "./Components/Project";
-import Navbar from "./Components/Navbar";
+import Home from "./Components/Home.jsx";
+import About from "./Components/About.jsx";
+import Projects from "./Components/Projects.jsx";
+import Project from "./Components/Project.jsx";
+import Navbar from "./Components/Navbar.jsx";
 
 function App() {
   return (
