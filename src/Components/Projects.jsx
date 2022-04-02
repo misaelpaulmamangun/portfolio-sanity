83c75796d4a8927ed25c1eb060c63dc6a54764da
Misael Paul <misaelpaulmamangun@gmail.com>
2022-04-02 21:30:50 +0800

    update image

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,9 +1,9 @@
-import BlockContent from "@sanity/block-content-to-react";
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import sanityClient from "../client.js";
+import BlockContent from '@sanity/block-content-to-react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import sanityClient from '../client.js'
 export default function Projects() {
-  const [projectsData, setProjects] = useState(null);
+  const [projectsData, setProjects] = useState(null)
 
   useEffect(() => {
     sanityClient
@@ -23,38 +23,38 @@ export default function Projects() {
       }`
       )
       .then((data) => {
-        setProjects(data);
+        setProjects(data)
       })
-      .catch(console.error);
-  }, []);
-  console.log(projectsData);
+      .catch(console.error)
+  }, [])
+  console.log(projectsData)
 
   return (
-    <div className="docs-section">
-      <div className="container">
+    <div className='docs-section'>
+      <div className='container'>
         <h1>Projects</h1>
-        <div className="row">
+        <div className='row'>
           {projectsData &&
             projectsData.map((project, index) => (
-              <div className="card four columns" key={project.slug.current}>
-                <div className="item-img">
+              <div className='card four columns' key={project.slug.current}>
+                <div className='item-img'>
                   <img
                     src={project.mainImage.asset.url}
                     alt={project.mainImage.alt}
                   />
                 </div>
-                <div className="item-desc">
-                  <div className="container">
-                    <Link to={{ pathname: project.link }} target="_blank">
-                      <h6 className="item-title text-center text-bold mt-1">
+                <div className='item-desc'>
+                  <div className='container'>
+                    <Link to={{ pathname: project.link }} target='_blank'>
+                      <h6 className='item-title text-center text-bold mt-1'>
                         {project.title}
                       </h6>
                     </Link>
                     <div>
                       <BlockContent
                         blocks={project.description}
-                        projectId="je7ph6zr"
-                        dataset="production"
+                        projectId='6kpzhc7k'
+                        dataset='production'
                       />
                     </div>
                   </div>
@@ -64,5 +64,5 @@ export default function Projects() {
         </div>
       </div>
     </div>
-  );
+  )
 }
