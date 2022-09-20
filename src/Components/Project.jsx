fe8a33d49e54b767de9854c3618d27fcf5a8806c
Misael Paul <79412408+misaelpaulmamangun@users.noreply.github.com>
2022-09-20 14:36:27 +0800

    Delete src directory

--- src/Components/Project.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Post() {
-  return (
-    <div className="docs-section">
-      <h1>Post</h1>
-    </div>
-  );
-}
--- src/client.js
+++
@@ -1,6 +0,0 @@
-import sanityClient from '@sanity/client';
-
-export default sanityClient({
-    projectId: "6kpzhc7k",
-    dataset: "production"
-})
